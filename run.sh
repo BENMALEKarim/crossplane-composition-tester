67ec54c30e54463d8a07f12f385394a59312ca45
BENMALEKarim <fk_benmalek@esi.dz>
2025-06-12 17:10:50 +0200

    add run script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker build -t crossplane-composition-tester:v0.0.1 --build-arg PYTHON_VERSION=3.12-slim --build-arg CROSSPLANE_VERSION=v1.17.3 .
+
+docker network create crossplane-net
+
+docker run --network crossplane-net -d --name function-environment-configs xpkg.upbound.io/crossplane-contrib/function-environment-configs:v0.1.0 --insecure --address=:9443
+docker run --network crossplane-net -d --name function-auto-ready xpkg.upbound.io/crossplane-contrib/function-auto-ready:v0.3.0 --insecure --address=:9443
+docker run --network crossplane-net -d --name function-go-templating xpkg.upbound.io/crossplane-contrib/function-go-templating:v0.7.0 --insecure --address=:9443
+
+docker run --rm --network crossplane-net -v ./test/:/app/test/ -v ./allure-results:/app/allure-results crossplane-composition-tester:v0.0.1 behave test/composition-tests -f allure_behave.formatter:AllureFormatter -o /app/allure-results -f pretty
