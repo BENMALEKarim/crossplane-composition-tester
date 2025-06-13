99eab5145adab939c14081692b40c45e0332ef62
BENMALEKarim <fk_benmalek@esi.dz>
2025-06-13 14:41:40 +0200

    update examples

--- examples/kubernetes/run.sh
+++ examples/kubernetes/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Install functions as deployment and expose them via service"
+echo "=== Install functions as deployment and expose them via service ==="
 kubectl apply -f functions.yaml
 
-echo "Launch crossplane-composition-testser job"
+echo "=== Launch crossplane-composition-testser job ==="
 kubectl apply -f crossplane-composition-tester.yaml
+
+kubectl wait --for=condition=complete job -l app=crossplane-composition-tester
+
+POD_NAME=$(kubectl get pods -l app=crossplane-composition-tester -oname)
+
+echo "=== Get logs of the crossplane-composition-tester"
+kubectl logs $POD_NAME
